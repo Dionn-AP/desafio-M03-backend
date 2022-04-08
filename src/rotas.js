9013774f5c138a481c713cf5483df70eda4e8458
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-04-08 13:04:44 -0300

    adicionando o middleware criado em index.js

--- src/rotas.js
+++ src/rotas.js
@@ -10,10 +10,12 @@ const rota = express();
 rota.post('/usuario', usuarios.cadastrarUsuario);
 rota.post('/login', usuarios.login);
 
+//AS ROTAS A PARTIR DESSE PONTO SERÃO VALIDADAS PELO MIDDLEWARE
 rota.use(validarLogin);
 
 rota.get('/usuario', usuarios.listarPerfilUsuarios);
-rota.put('/usuario/:id', usuarios.atualizarUsuario);
+rota.put('/usuario/', usuarios.atualizarUsuario);
+
 
 //ROTAS TRANSAÇÕES
 rota.get('/transacao', );
@@ -21,6 +23,7 @@ rota.get('/transacao/:id', );
 rota.post('/transacao', );
 rota.put('/transacao/:id', );
 rota.delete('/transacao/:id', );
+
 
 //ROTAS CATEGORIA
 rota.get('/categoria', categorias.listarCategoria);
