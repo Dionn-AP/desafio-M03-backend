3ea3b568f6502ffe7bc30f67715acfebc9b93bdf
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-04-11 20:59:25 -0300

    adicionando as validacoes e refatorando a funcao listarCategorias

--- src/controladores/categorias.js
+++ src/controladores/categorias.js
@@ -4,19 +4,21 @@ const conexao = require('../conexao');
 const listarCategoria = async (req, res) => {
     const { usuario } = req;
 
-    const query = `select * from categorias`;
-
     try {
-        const {rows: categorias} = await conexao.query(query);
+        const query = `
+        select distinct c.id, c.descricao from categorias c 
+        inner join transacoes t on c.id = t.categoria_id 
+        where t.usuario_id = $1
+        `;
 
-        if(!categorias.length) {
-            return res.status(404).json()
-        }
+        const categorias = await conexao.query(query, [usuario.id]);
 
-        return res.status(200).json(categorias);
+        return res.status(200).json(categorias.rows);
 
     } catch (error) {
-        return res.status(500).json({"mensagem": "Falha na consulta. Não foi possivel listar as categorias."})
+        return res.status(500).json({
+            "mensagem": "Falha na consulta. Não foi possivel listar as categorias."
+        });
     }
 
     
